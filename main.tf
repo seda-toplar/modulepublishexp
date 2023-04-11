590000f531c93e38e567620290fa6c52661f6e81
S56921 <fuckapple@Seda-MBP.fritz.box>
2023-04-11 18:40:10 +0200

    update policy

--- main.tf
+++ main.tf
@@ -24,34 +24,10 @@ resource "azurerm_policy_definition" "example" {
   policy_type  = "Custom"
   mode         = "All"
   display_name = "my-policy-definition"
-  policy_rule  = <<POLICY_RULE
-                    {
-                    "if": {
-                    "not": {
-                        "field": "location",
-                        "equals": "westeurope"
-                    }
-                    },
-                    "then": {
-                    "effect": "Deny"
-                    }
-                }
-                POLICY_RULE
-}
+ }
 
 resource "azurerm_resource_group_policy_assignment" "example" {
   name                 = "example"
   resource_group_id    = azurerm_resource_group.rg.id
   policy_definition_id = azurerm_policy_definition.example.id
-
-  parameters = <<PARAMS
-        {
-        "tagName": {
-            "value": "Business Unit"
-        },
-        "tagValue": {
-            "value": "BU"
-        }
-        }
-    PARAMS
 }
